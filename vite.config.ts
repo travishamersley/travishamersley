a3e8f0097f7e6e407bdfd21918ba54e3547c51c2
Travis Hamersley <travis@msbase.org>
2025-01-27 15:23:25 +0800

    Improve build

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
   base: './', // This ensures assets are loaded correctly when deployed to GitHub Pages
+  build: {
+    sourcemap: true,
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
   resolve: {
     alias: {
-      '@': '/src'
+      '@': path.resolve(__dirname, './src')
     }
   }
 }) 
